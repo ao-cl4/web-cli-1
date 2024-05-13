fc7f1259280803e87a9c98a87dc992cd0b7558a7
ao-cl4 <ao@cl4.ai>
2024-05-13 20:24:13 +0000

    push 4

---
+++ config/help.js
@@ -0,0 +1,17 @@
+const help = {
+  "%help%": "list all commands (you're looking at it)",
+  "%whois%": "list all partners",
+  "%whois% [partner]": "learn about a partner",
+  "%whois% root": "learn about us",
+  "%tldr%": "list all portfolio companies",
+  "%tldr% [company_name]": "learn about a portfolio company",
+  "%email%": "reach out to us",
+  "%twitter%": "twitter accounts",
+  "%instagram%": "instagram account",
+  "%git%": "this repo",
+  "%github%": "all repos",
+  "%locate%": "physical address",
+  "%jobs%": "check out our job openings",
+  "%test%": "do not use",
+  "%other%": "try your fav commands (e.g. %ls%, %groups%, %su%)"
+};
